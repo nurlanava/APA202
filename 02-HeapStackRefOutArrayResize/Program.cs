bdeb2cd8b98bd0f47297710e8ac484ab8e19d6cf
nurlanava <nurlanava-apa202@code.edu.az>
2026-03-17 15:22:37 +0400

    02-HeapStackRefOutArrayResize

---
+++ 02-HeapStackRefOutArrayResize/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters;
+class Porgram
+{
+static int[] ArrayResize(int[] numbers,params int[] values)
+    {
+        Array.Resize(ref numbers, numbers.Length,params int[] values);
+        for(int i=0; i<numbers.Length; i++ )    
+        {
+            numbers[numbers.Length-values.Length +i] = values[i];
+    }
+        return numbers;
+        static void Main()
+        {
+            int[] numbers = { 5, 6, 7 };
+            numbers=ArrayResize(numbers, 8,9);
+            For(int i = 0;int<numbers.Length;int++)
+                {
+                Console.WriteLine(numbers[i]+" ");
+            }
+        }
