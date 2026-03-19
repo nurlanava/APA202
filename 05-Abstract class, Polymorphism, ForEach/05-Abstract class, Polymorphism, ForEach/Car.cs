f4a9c7b7ab678d79e078913191dfc0a6ad98234c
nurlanava <nurlanava-apa202@code.edu.az>
2026-03-19 14:00:11 +0400

    Add files via upload

---
+++ 05-Abstract class, Polymorphism, ForEach/05-Abstract class, Polymorphism, ForEach/Car.cs
@@ -0,0 +1,28 @@
+﻿class Car : Vehicle
+{
+    public int DoorsCount;
+    public int TrunkCapacity;
+    public bool IsAutomatic;
+    public int MaxSpeed;
+
+    public Car(string brand, string model, int year, string plateNumber,
+        int doors, int trunk, bool isAuto, int maxSpeed)
+        : base(brand, model, year, plateNumber)
+    {
+        this.DoorsCount = doors;
+        this.TrunkCapacity = trunk;
+        this.IsAutomatic = isAuto;
+        this.MaxSpeed = maxSpeed;
+    }
+
+    public void ShowCarInfo()
+    {
+        ShowBasicInfo();
+        Console.WriteLine($"Doors:{DoorsCount}, Trunk:{TrunkCapacity}, Automatic:{IsAutomatic}, MaxSpeed:{MaxSpeed}");
+    }
+
+    public double CalculateFuelCost(double distance)
+    {
+        return (distance / 100) * 8 * 1.50;
+    }
+}
